8ea80ed5cb9de6b2b1e514c63d642797ff930334
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-07-14 22:30:42 -0700

    3rd option

--- VariablesDataTypes/Program.cs
+++ VariablesDataTypes/Program.cs
@@ -12,6 +12,7 @@ namespace VariablesDataTypes
         {
             Console.WriteLine("Which program would you like to Run:");
             Console.WriteLine("For Car Insurance, enter c");
+            Console.WriteLine("For Daily Report, enter d");
             Console.WriteLine("For Income Comparison, enter i");
             char myChoice = Convert.ToChar(Console.ReadLine());
             Console.WriteLine("=============================================================");
@@ -20,6 +21,9 @@ namespace VariablesDataTypes
             {
                 case 'c':
                     carInsurance();
+                    break;
+                case 'd':
+                    dailyReport();
                     break;
                 case 'i':
                     incomeProgram();
@@ -88,48 +92,50 @@ namespace VariablesDataTypes
         }
 
 
-            ////basic operations O
-            //Console.WriteLine("Input a number to multiply by 50");
-            ////combined read and write onto one line:
-            //Console.WriteLine(Convert.ToInt32(Console.ReadLine()) * 50);
-            //Console.WriteLine("Input a number to add to 25");
-            //Console.WriteLine(Convert.ToInt32(Console.ReadLine()) * 25);
-            //Console.WriteLine("Input a number to divide by 12.5");
-            //Console.WriteLine(Convert.ToDecimal(Console.ReadLine()) / 12.5m);
-            //Console.WriteLine("Input a number to check if greater than 50");
-            //Console.WriteLine(Convert.ToInt32(Console.ReadLine()) > 50);
-            //Console.WriteLine("Input a number to divide by 7 and print remainder");
-            //Console.WriteLine(Convert.ToDecimal(Console.ReadLine()) % 7);
-            ////end of program
-            //Console.WriteLine("thats all");
-            //Console.ReadLine();
+        ////basic operations O
+        //Console.WriteLine("Input a number to multiply by 50");
+        ////combined read and write onto one line:
+        //Console.WriteLine(Convert.ToInt32(Console.ReadLine()) * 50);
+        //Console.WriteLine("Input a number to add to 25");
+        //Console.WriteLine(Convert.ToInt32(Console.ReadLine()) * 25);
+        //Console.WriteLine("Input a number to divide by 12.5");
+        //Console.WriteLine(Convert.ToDecimal(Console.ReadLine()) / 12.5m);
+        //Console.WriteLine("Input a number to check if greater than 50");
+        //Console.WriteLine(Convert.ToInt32(Console.ReadLine()) > 50);
+        //Console.WriteLine("Input a number to divide by 7 and print remainder");
+        //Console.WriteLine(Convert.ToDecimal(Console.ReadLine()) % 7);
+        ////end of program
+        //Console.WriteLine("thats all");
+        //Console.ReadLine();
 
-            ////Some math M
-            //Console.WriteLine("6 + 2 = " + (6 + 2));
-            //Console.WriteLine("6 - 2 = " + (6 - 2));
-            //Console.WriteLine("6 x 2 = " + (6 * 2));
-            //Console.WriteLine("6 / 2 = " + (6 / 2));
-            //int product = 10 * 5;
-            //Console.WriteLine(product);
+        ////Some math M
+        //Console.WriteLine("6 + 2 = " + (6 + 2));
+        //Console.WriteLine("6 - 2 = " + (6 - 2));
+        //Console.WriteLine("6 x 2 = " + (6 * 2));
+        //Console.WriteLine("6 / 2 = " + (6 / 2));
+        //int product = 10 * 5;
+        //Console.WriteLine(product);
 
-
-            ////DAILY REPORT PROMPTS D
-            //Console.WriteLine("The Tech Academy");
-            //Console.WriteLine("Student Daily Report");
-            //Console.WriteLine("Enter Name:");
-            //string yourName = Console.ReadLine();
-            //Console.WriteLine("Enter Current Course");
-            //string yourCourse = Console.ReadLine();
-            //Console.WriteLine("Enter Page Number:");
-            //int yourPage = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Do you need help with anything? Enter true or false");
-            //bool needHelp = Convert.ToBoolean(Console.ReadLine());
-            //Console.WriteLine("Any positives you'd like to share?");
-            //string yourWins = Console.ReadLine();
-            //Console.WriteLine("Anything else?");
-            //string yourDetails = Console.ReadLine();
-            //Console.WriteLine("Hours studied today:");
-            //byte yourHours = Convert.ToByte(Console.ReadLine());
+        private static void dailyReport()
+        {
+            //DAILY REPORT PROMPTS D
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("Enter Name:");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Enter Current Course");
+            string yourCourse = Console.ReadLine();
+            Console.WriteLine("Enter Page Number:");
+            int yourPage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Enter true or false");
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Any positives you'd like to share?");
+            string yourWins = Console.ReadLine();
+            Console.WriteLine("Anything else?");
+            string yourDetails = Console.ReadLine();
+            Console.WriteLine("Hours studied today:");
+            byte yourHours = Convert.ToByte(Console.ReadLine());
+        }
 
             ////end of program message
             //Console.WriteLine("\r Thank You for your answers. An instructor will respond shortly.");
